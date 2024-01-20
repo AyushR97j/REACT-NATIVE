cba16a76a257726378ffdc89de05c70f4ec3c0e2
AyushR97j <ayushraj803110@gmail.com>
2024-01-20 15:53:04 +0530

    style inheritance

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -3,8 +3,12 @@ import { View, Text, StyleSheet } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={[styles.darkMode]}>
+        <Text style={styles.darkModeText}>Style inheritance
+          <Text style={styles.boldText}>in bold</Text></Text>
+      </View>
       <View style={[styles.lightblueBg, styles.box]}>
-        <Text>Lightblue Box</Text>
+        <Text style={ { borderRadius: 5}}>Lightblue Box</Text>
       </View>
       <View style={[styles.lightgreenBg, styles.box, styles.boxShadow, styles.androidShadow]}>
         <Text>Lightgreen Box</Text>
@@ -15,7 +19,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: "plum", padding: 60 },
-  box: {width: 250, height: 250, padding: 10, margin: 6},
+  darkMode: {
+    backgroundColor: "black",
+    color: "white",
+  },
+  darkModeText: {
+    color: "white",
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  box: {width: 250, height: 250, padding: 10, margin: 6, borderWidth: 2, borderRadius: 5},
   lightblueBg: {backgroundColor: "lightblue"},
   lightgreenBg: {backgroundColor: "lightgreen"},
   boxShadow: {
