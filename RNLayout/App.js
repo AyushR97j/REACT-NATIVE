02d6f10a2b31524754c7dc84a2b04053389ee0bc
AyushR97j <ayushraj803110@gmail.com>
2024-01-20 20:50:36 +0530

    Layout completed

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -5,11 +5,11 @@ export default function App() {
   return (
     // <View style={{ backgroundColor: "plum", flex: 1 }}>
     <View style={styles.container}>
-      <Box style={ { backgroundColor: "#8e9b00", flex: 1 }}>Box 1</Box>
+      <Box style={ { backgroundColor: "#8e9b00" }}>Box 1</Box>
       <Box style={ { backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={ { backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={ { backgroundColor: "#6b0803", flex: 1 }}>Box 4</Box>
-      <Box style={ { backgroundColor: "#8e9b00" }}>Box 5</Box>
+      <Box style={ { backgroundColor: "#6b0803" }}>Box 4</Box>
+      <Box style={ { backgroundColor: "#8e9b00", alignSelf: "center" }}>Box 5</Box>
       <Box style={ { backgroundColor: "#8f9b76" }}>Box 6</Box>
     </View>
   );
@@ -17,6 +17,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: "space-evenly",
+    alignItems: "stretch",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
