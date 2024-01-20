5b8d0e231d5d5789968fdc4b0908300530b162f9
AyushR97j <ayushraj803110@gmail.com>
2024-01-20 13:35:51 +0530

    multiple styles

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -3,11 +3,20 @@ import { View, Text, StyleSheet } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>StyleSheet API</Text>
+      <View style={[styles.lightblueBg, styles.box]}>
+        <Text>Lightblue Box</Text>
+      </View>
+      <View style={[styles.lightgreenBg, styles.box]}>
+        <Text>Lightgreen Box</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: "plum", padding: 60 }
+  container: {flex: 1, backgroundColor: "plum", padding: 60 },
+  box: {width: 100, height: 100, padding: 10},
+  lightblueBg: {backgroundColor: "lightblue"},
+  lightgreenBg: {backgroundColor: "lightgreen"},
+
 })
