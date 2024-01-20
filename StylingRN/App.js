de520c2b739ca31d52f02887b163a74d22a06e81
AyushR97j <ayushraj803110@gmail.com>
2024-01-20 15:32:43 +0530

    Box model && shadow && elevation

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -6,7 +6,7 @@ export default function App() {
       <View style={[styles.lightblueBg, styles.box]}>
         <Text>Lightblue Box</Text>
       </View>
-      <View style={[styles.lightgreenBg, styles.box]}>
+      <View style={[styles.lightgreenBg, styles.box, styles.boxShadow, styles.androidShadow]}>
         <Text>Lightgreen Box</Text>
       </View>
     </View>
@@ -15,8 +15,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: "plum", padding: 60 },
-  box: {width: 100, height: 100, padding: 10},
+  box: {width: 250, height: 250, padding: 10, margin: 6},
   lightblueBg: {backgroundColor: "lightblue"},
   lightgreenBg: {backgroundColor: "lightgreen"},
+  boxShadow: {
+    shadowColor: "#333333",
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 0.9,
+    shadowRadius: 4,
+  },
+  androidShadow: {
+    elevation: 10,
+  },
 
 })
