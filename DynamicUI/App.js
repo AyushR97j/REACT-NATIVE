4e5d147a3b4d8d3669eaee45ce73d3b6ffd01130
AyushR97j <ayushraj803110@gmail.com>
2024-01-21 10:59:34 +0530

    Platform Specific code

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import CustomButton from './components/CustomButton/CustomButton';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
         <View style={styles.box}>
-          <Text style={styles.text}>Welcome</Text> 
+          <Text style={styles.text}>Welcome</Text>
+          <CustomButton title="Press Me" onPress={() => alert("Pressed")}/>
         </View>
       </View>
     </SafeAreaView>
@@ -21,12 +23,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'plum',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
   box : {
     padding: 20,
   },
   text: {
-    fontSize: 24,
+    ...Platform.select({
+      ios: {
+        color: "purple",
+        fontSize: 24,
+        fontStyle: "italic",
+      },
+      android: {
+        color: "blue",
+        fontSize: 30,
+        fontStyle: "italic",
+      },
+    }),
     fontWeight: "bold",
     textAlign: "center",
   },
