0cd0ad5bb082aa1eebf7d5a300768299faa8f4d4
AyushR97j <ayushraj803110@gmail.com>
2024-01-21 09:28:26 +0530

    Dynamic UI - useWindowDimensions

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,19 +1,24 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 
 export default function App() {
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome</Text> 
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          }
+        ]}>
+        <Text style={{ fontSize : windowWidth > 500 ? 50 : 24}}>Welcome</Text> 
       </View>
     </View>
   );
 }
-
-const windowWidth = Dimensions.get("window").width;
-const windowHeight= Dimensions.get("window").height;
-
-console.log(windowWidth, windowHeight);
 
 const styles = StyleSheet.create({
   container: {
@@ -23,14 +28,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box : {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 500 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
-
   },
 });
