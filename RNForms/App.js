8c10dac431a357e832a16cc97df3ff521b01e19a
AyushR97j <ayushraj803110@gmail.com>
2024-01-24 00:52:28 +0530

    Login Form

--- RNForms/App.js
+++ RNForms/App.js
@@ -1,61 +1,65 @@
-import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput, Switch } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState("")
-  const [isDarkMode, setisDarkMode] = useState(false)
+  const [ username, setUsername] = useState("")
+  const [ password, setPassword] = useState("")
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput 
-      style={styles.input} 
-      value={name} 
-      onChangeText={setName} 
-      placeholder='email..'
-      autoCorrect={false}
-      autoCapitalize='none'
-      // secureTextEntry
-      // keyboardType='numeric'
-      />
-      <TextInput style={[styles.input,styles.multilineText]} placeholder='message' multiline />
-      <Text style={styles.text}>My name is {name}</Text>
-
-      <View style={styles.switchContainer}>
-        <Text style={styles.text}>Dark Mode</Text>
-        <Switch 
-          value={isDarkMode} 
-          onValueChange={() => setisDarkMode((previousState) => !previousState)}
-          trackColor={{false: "767577", true: "lightblue"}}
-          thumbColor="#f4f3f4"
-        />
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.label}>Username</Text>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Enter your usernmae" 
+          value={username}
+          onChangeText={setUsername} 
+          />
+        <Text style={styles.label}>Password</Text>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Enter your password" 
+          secureTextEntry
+          value={password}
+          onChange={setPassword}
+          />
+        <Button title="login"/>
       </View>
-    </SafeAreaView>
+    </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "f5f5f5",
+  },
+  form: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: "bold",
   },
   input: {
     height: 40,
-    margin: 12,
-    padding: 10,
+    borderColor: "#ddd",
     borderWidth: 1,
-  },
-  text: {
-    fontSize: 30,
+    marginBottom: 15,
     padding: 10,
-  },
-  multilineText: {
-    minHeight: 100,
-    textAlignVertical: "top"
-  },
-  switchContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 10,
+    borderRadius: 5,
   }
 });
