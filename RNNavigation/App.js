5383919986e9f0eaea01ed5b97a282c5d8995a6e
AyushR97j <ayushraj803110@gmail.com>
2024-01-25 01:06:22 +0530

    Drawer navigation

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -1,46 +1,18 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import AboutScreen from "./screens/AboutScreen";
-import { Pressable, Text } from "react-native";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import DashboardScreen from './screens/DashboardScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#6a51ae",
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: { fontWeight: "bold" },
-          headerRight: () => (
-            <Pressable onPress={() => alert("Menu Button Pressed")}>
-              <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
-            </Pressable>
-          ),
-          contentStyle: {
-            backgroundColor: "#e8e4f",
-          },
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: "Welcome Home",
-          }}
-        />
-        <Stack.Screen
-          name="About"
-          component={AboutScreen}
-          initialParams={{
-            name: "Guest",
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen name="Dashboard" component={DashboardScreen} />
+                <Drawer.Screen name="Settings" component={SettingsScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    )
 }
