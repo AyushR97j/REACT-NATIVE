6b3e2e681ce325a476f44746e5be2361024b0e76
AyushR97j <ayushraj803110@gmail.com>
2024-01-25 00:41:21 +0530

    dynamic navigation

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -1,8 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import AboutScreen from './screens/AboutScreen';
-import { Pressable, Text } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+import { Pressable, Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,35 +10,35 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-              backgroundColor: "#6a51ae"
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {fontWeight: "bold"},
-            headerRight: () => (
-              <Pressable onPress={() => alert("Menu Button Pressed")}>
-                <Text style={ {color: "#fff", fontSize: 16}}>Menu</Text>
-              </Pressable>
-            ),
-            contentStyle: {
-              backgroundColor: "#e8e4f"
-            }
-      }}>
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#6a51ae",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          headerRight: () => (
+            <Pressable onPress={() => alert("Menu Button Pressed")}>
+              <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: "#e8e4f",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
           options={{
             title: "Welcome Home",
-            
-          }}   
+          }}
         />
-        <Stack.Screen 
-          name="About" 
+        <Stack.Screen
+          name="About"
           component={AboutScreen}
           initialParams={{
             name: "Guest",
-          }} 
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
