b62196a60d61316158427494e28f3bd8e373067d
AyushR97j <ayushraj803110@gmail.com>
2024-01-24 19:45:06 +0530

    passing data between screens

--- RNNavigation/screens/AboutScreen.js
+++ RNNavigation/screens/AboutScreen.js
@@ -1,9 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export default function AboutScreen() {
+export default function AboutScreen( {navigation, route} ) {
+  const { name } = route.params;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About Screen</Text>
+      <Text style={styles.text}>About {name}</Text>
+      <Button
+        title="Update the name"
+        onPress={() => navigation.setParams({ name: "Ayush Rj" })}
+      />
+      <Button
+        title="Go back with data"
+        onPress={() => {
+          navigation.navigate("Home", { result: "Data from About" });
+        }}
+      />
     </View>
   );
 }
